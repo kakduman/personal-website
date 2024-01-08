1d8883785f2f9490d9f856136603c3469719de11
Purpur <43528123+Pemigrade@users.noreply.github.com>
2024-01-08 17:17:51 -0600

    is this going to break anything?

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -14,7 +14,7 @@ function AboutSection() {
                 <p>Hi, I'm Koray. I'm a junior at Yale majoring in Computer Science. I also run Birdflop, the only 501(c)3 nonprofit Minecraft server host designed to foment interest in computer science and technology. I'm passionate about machine learning, open-source software, and hiking. I also love mulling cider and over the best zeugmas (pun intended)!
                     <br/>
                     <br/>
-                    Most recently, I started working on my personal website (on December 24). There's lots to come...</p>
+                    Most recently, I started working on my personal website (on December 24) using React with Typescript. There's lots to come...</p>
                 <br/>
                 <div>
                     <a href="#projects-section" className="btn-large">View Projects</a>
